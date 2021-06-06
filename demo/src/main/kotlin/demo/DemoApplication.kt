43247d71c538b4ee37af173dc2585b04cbad1e2d
ddojai <favianred@gmail.com>
2021-06-06 18:38:55 +0900

    Add a database to a Spring Boot RESTful web service
    - https://kotlinlang.org/docs/jvm-spring-boot-restful-db.html

--- demo/src/main/kotlin/demo/DemoApplication.kt
+++ demo/src/main/kotlin/demo/DemoApplication.kt
@@ -2,7 +2,13 @@ package demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
@@ -13,13 +19,22 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageResource {
+class MessageResource(val service: MessageService) {
     @GetMapping
-    fun index(): List<Message> = listOf(
-            Message("1", "Hello!"),
-            Message("2", "Bonjour!"),
-            Message("3", "Privet!"),
-    )
+    fun index(): List<Message> = service.findMessages()
+
+    @PostMapping
+    fun post(@RequestBody message: Message) {
+        service.post(message)
+    }
 }
 
-data class Message(val id: String?, val text: String)
+
+@Table("MESSAGES")
+data class Message(@Id val id: String?, val text: String)
+
+interface MessageRepository : CrudRepository<Message, String> {
+
+    @Query("select * from messages")
+    fun findMessages(): List<Message>
+}
